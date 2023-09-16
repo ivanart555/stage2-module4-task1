140090e411ae748867b2bb0e8a7367c6abf64bb4
Ivan Artsiushyn <ivanart555@gmail.com>
2023-09-16 17:06:03 +0300

    Solve tasks

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,23 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    // Write your code here!
+    private static final Object mutex = new Object();
+    private static volatile ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {
+    }
+
+    public static ThreadSafeSingleton getInstance() {
+        ThreadSafeSingleton result = instance;
+        if (result == null) {
+            synchronized (mutex) {
+                result = instance;
+                if (result == null)
+                    instance = result = new ThreadSafeSingleton();
+            }
+        }
+        return result;
+
+    }
+
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,29 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    private static final Logger LOGGER = Logger.getLogger(H2ConnectionFactory.class.getName());
+
+    @Override
+    public Connection createConnection() {
+        try {
+            Properties properties = new Properties();
+            properties.load(
+                    H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
+
+            return DriverManager.getConnection(properties.getProperty("postgres.url"), properties.getProperty("postgres.name"), properties.getProperty("postgres.password"));
+        } catch (IOException | SQLException e) {
+            LOGGER.warning("Failed to get connection to database!");
+        }
+        return null;
+    }
 }
 
